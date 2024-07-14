b46503af69d52043e404dad3afa303474d1fc4f6
devbrayan2007 <devbrayan2007@gmail.com>
2024-07-14 13:53:00 -0300

    aula011

---
+++ Interfaces/aula011.php
@@ -0,0 +1,29 @@
+<?php 
+
+
+interface Crud{
+    public function create();
+    public function read();
+    public function update();
+    public function delete();
+}
+
+class Noticias implements Crud{
+
+    public function create(){
+        // lógica para criar uma notícia
+    }
+
+    public function read(){
+        // lógica para ler uma notícia
+    }
+
+    public function update(){
+        // lógica para atualizar uma notícia
+    }
+
+    public function delete(){
+        // lógica para deletar uma notícia
+        
+    }
+}
