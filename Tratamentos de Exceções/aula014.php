11db67c25ff03f7ca83047675a3940dafce4139f
devbrayan2007 <devbrayan2007@gmail.com>
2024-07-15 11:04:36 -0300

    aula014

--- Tratamentos de Exceções/aula014.php
+++ Tratamentos de Exceções/aula014.php
@@ -1,3 +1,27 @@
 <?php 
 
+class Login{
 
+    public function cadastrarEmail($email){
+
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            throw new Exception("Este email é inválido!", 1);
+        }
+        else{
+            echo "Email cadastrado com sucesso!";
+        }
+    }
+}
+
+
+$login = new Login();
+
+try{
+    $login->cadastrarEmail("brayanrcampos");
+}
+catch(Exception $e){
+    echo "Mensagem: ".$e->getMessage() . "<br>";
+    echo "Código: ".$e->getCode(). "<br>";
+    echo "Linha: ".$e->getLine(). "<br>";
+    echo "Arquivo: ".$e->getFile(). "<br>";
+}
